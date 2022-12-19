6a504ddec201415981df4a78eeab3d0a18c105fc
spk <ashutoshkarande5@gmail.com>
2022-12-19 23:16:53 +0530

    31

--- 01-app-demo/src/App.js
+++ 01-app-demo/src/App.js
@@ -5,24 +5,32 @@ import './App.css';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      name: { firstName: 'Aashutosh', lastName: 'Karande' }
+    }
+  }
+
+
   render() {
     return (
       <div className="App" >
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            hello my name is Aashu12345
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p> Hi {this.state.name.firstName} {this.state.name.lastName} </p>
+          <button onClick={() => {
+            this.setState((state, props) => {
+              return {
+                name: { firstName: 'Spk' }
+              }
+            }, () => {
+              console.log(this.state)
+            })
+          }}
+          >Change Name</button>
         </header>
-      </div>
+      </div >
     );
   };
 };

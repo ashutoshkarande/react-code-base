40cf007548a4d0e72dcd652e1f0cdeddfe2e668f
spk <ashutoshkarande5@gmail.com>
2022-12-20 00:52:41 +0530

    40

--- 01-app-demo/src/App.js
+++ 01-app-demo/src/App.js
@@ -1,36 +1,77 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
   constructor() {
     super();
+    // this.state = {
+    //   name: { firstName: 'Aashutosh', lastName: 'Karande' }
+    // }
+    /*
+        {
+          id: 'abc123',
+          name: 'Linda',
+        }
+    */
+
     this.state = {
-      name: { firstName: 'Aashutosh', lastName: 'Karande' }
+      monsters: []
     }
+
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users, fullList: users }
+      },
+        () => {
+          console.log(this.state)
+        }
+      ));
+
+    console.log('componentDidMount');
   }
 
 
+  myFunction() {
+    console.log('myFunction');
+  }
+
   render() {
+    console.log('render');
+
     return (
-      <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p> Hi {this.state.name.firstName} {this.state.name.lastName} </p>
-          <button onClick={() => {
-            this.setState((state, props) => {
-              return {
-                name: { firstName: 'Spk' }
-              }
-            }, () => {
+      <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          console.log(event.target.value);
+          var filteredMonsters = this.state.fullList.filter((f) => {
+            return f.name.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase());
+          });
+          this.setState(() => {
+            return {
+              monsters: filteredMonsters
+            }
+          },
+            () => {
               console.log(this.state)
-            })
-          }}
-          >Change Name</button>
-        </header>
-      </div >
+            }
+          );
+
+
+        }} />
+        {
+          this.state.monsters.map((monster) => {
+            return <div key={monster.id}><h1>{monster.name}</h1></div>
+          })
+        }
+        <button onClick={this.myFunction}>Hi</button>
+      </div>
     );
   };
 };
@@ -55,5 +96,24 @@ class App extends Component {
 //     </div>
 //   );
 // }
+
+/*
+
+<header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p> Hi {this.state.name.firstName} {this.state.name.lastName} </p>
+          <button onClick={() => {
+            this.setState((state, props) => {
+              return {
+                name: { firstName: 'Spk' }
+              }
+            }, () => {
+              console.log(this.state)
+            })
+          }}
+          >Change Name</button>
+        </header>
+
+        */
 
 export default App;

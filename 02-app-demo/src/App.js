6e25f8d0419c49a7cbfc64d352635d48cd8cb9c7
spk <ashutoshkarande5@gmail.com>
2022-12-26 12:45:32 +0530

    85

--- 02-app-demo/src/App.js
+++ 02-app-demo/src/App.js
@@ -1,38 +1,34 @@
 
+import { Routes, Route, Outlet } from 'react-router-dom';
 
-import Directory from './components/directory/directory.component';
-const App = () => {
+import Home from './routes/home/home.component';
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
 
+const Navigation = () => {
   return (
-    <Directory categories={categories} />
+    <div>
+      <div>
+        <h1>I am the Nav bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () => {
+  return (
+    <h1>I am a shop</h1>
+  )
+}
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />} >
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
 }
 

df88d3fe98c304781474f04eb2f3eb486508e4f5
spk <ashutoshkarande5@gmail.com>
2022-12-28 00:26:50 +0530

    98

--- 02-app-demo/src/routes/sign-in/sign-in.component.jsx
+++ 02-app-demo/src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,25 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from './../../utils/firebase/firebase.utils'
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+    signInWithGoogleRedirect,
+    auth
+} from './../../utils/firebase/firebase.utils'
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.componant';
 
 const SignIn = () => {
+
+    useEffect(() => {
+        (async () => {
+            const response = await getRedirectResult(auth);
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+        })();
+    }, []);
 
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopup();
@@ -10,9 +29,8 @@ const SignIn = () => {
     return (
         <div>
             <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
-                Sign in with Google
-            </button>
+            <button onClick={logGoogleUser}> Sign in with Google </button>
+            <SignUpForm />
         </div>
     )
 }
